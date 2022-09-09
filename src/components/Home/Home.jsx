1f70a7e27ad755820b1dc2735a06167ae5667f8b
connorpeck <90285369+connorpeck@users.noreply.github.com>
2022-09-09 09:41:57 -0500

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+    return (
+      <div className="Home">
+   
+          <p>
+           This is My Home Page
+          </p>
+          
+      </div>
+    );
+  }
+  
+  export default Home;
